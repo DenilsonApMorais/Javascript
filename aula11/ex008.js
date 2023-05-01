49391f0847c7b1f909e4cd5e3f6e4c792bb42956
denilson <denilsonapmorais@gmail.com>
2023-04-30 23:51:34 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é: ${vel} km/h`)
+
+if(vel >= 60){
+    console.log("Voce fou multado!")
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "eua"
+
+console.log(`vivendo em ${pais} você : `)
+if (pais =="brasil"){
+    console.log("Brasileiro")
+}else{
+    console.log("Não é brasileiro")
+}
